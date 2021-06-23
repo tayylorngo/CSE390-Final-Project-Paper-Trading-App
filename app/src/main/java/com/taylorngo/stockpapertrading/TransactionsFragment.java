75e352183e09b12cde83d5eb66e0dadb944b4f00
Taylor Ngo <47530918+tayylorngo@users.noreply.github.com>
2021-06-23 02:49:45 -0400

    added navigation bar

---
+++ app/src/main/java/com/taylorngo/stockpapertrading/TransactionsFragment.java
@@ -0,0 +1,19 @@
+package com.taylorngo.stockpapertrading;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class TransactionsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_transactions, container, false);
+    }
+}
