245bd928af5fdfb391103ca0fcea20a73cae4cc5
Taylor Ngo <47530918+tayylorngo@users.noreply.github.com>
2021-06-25 09:25:37 -0400

    created database

---
+++ app/src/main/java/com/taylorngo/stockpapertrading/StocksContract.java
@@ -0,0 +1,21 @@
+package com.taylorngo.stockpapertrading;
+
+import android.provider.BaseColumns;
+
+public class StocksContract {
+
+    private StocksContract(){}
+
+    public static final class StockEntry implements BaseColumns {
+        public static final String TABLE_NAME = "stocksList";
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_AMOUNT = "amount";
+        public static final String COLUMN_TIMESTAMP = "timestamp";
+    }
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/taylorngo/stockpapertrading/StocksDBHelper.java
@@ -0,0 +1,36 @@
+package com.taylorngo.stockpapertrading;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.taylorngo.stockpapertrading.StocksContract.*;
+
+import androidx.annotation.Nullable;
+
+public class StocksDBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "stockslist.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public StocksDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String SQLITE_CREATE_STOCKSLIST_TABLE = "CREATE TABLE " +
+                StockEntry.TABLE_NAME + " (" +
+                StockEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                StockEntry.COLUMN_NAME + " TEXT NOT NULL, " +
+                StockEntry.COLUMN_AMOUNT + " INTEGER NOT NULL, " +
+                StockEntry.COLUMN_TIMESTAMP + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
+                ");";
+        db.execSQL(SQLITE_CREATE_STOCKSLIST_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + StockEntry.TABLE_NAME);
+        onCreate(db);
+    }
+}
