f4533f94b4c6c97c3e1eab075683dababc656a36
Taylor Ngo <47530918+tayylorngo@users.noreply.github.com>
2021-06-28 01:42:24 -0400

    fixed withdraw funds

--- app/src/main/java/com/taylorngo/stockpapertrading/MainActivity.java
+++ app/src/main/java/com/taylorngo/stockpapertrading/MainActivity.java
@@ -80,8 +80,8 @@ public class MainActivity extends AppCompatActivity implements AddFundsDialog.Ad
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("balance", String.valueOf(newBalance));
         editor.apply();
-        Toast toast = Toast.makeText(this, "Withdrew $" + amount, Toast.LENGTH_SHORT);
-        toast.show();
+//        Toast toast = Toast.makeText(this, "Withdrew $" + amount, Toast.LENGTH_SHORT);
+//        toast.show();
         currFrag.closeWithdrawDialog();
     }
 }
--- app/src/main/java/com/taylorngo/stockpapertrading/WithdrawFundsDialog.java
+++ app/src/main/java/com/taylorngo/stockpapertrading/WithdrawFundsDialog.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -47,6 +48,9 @@ public class WithdrawFundsDialog extends AppCompatDialogFragment {
             }
         });
 
+        TextView currBalanceLabel = view.findViewById(R.id.withdrawDialogCurrBalanceLabel);
+        currBalanceLabel.setText("Withdrawable Balance: $" + currBalance);
+
         EditText fundsInput = view.findViewById(R.id.withdrawFundsInput);
         fundsInput.addTextChangedListener(new TextWatcher() {
             @Override
@@ -69,8 +73,8 @@ public class WithdrawFundsDialog extends AppCompatDialogFragment {
                     return;
                 }
                 if(Double.parseDouble(fundsInput.getText().toString()) > currBalance){
-                    Toast toast = Toast.makeText(getContext(), "Not enough funds", Toast.LENGTH_SHORT);
-                    toast.show();
+//                    Toast toast = Toast.makeText(getContext(), "Not enough funds", Toast.LENGTH_SHORT);
+//                    toast.show();
                     return;
                 }
                 listener.applyTexts(Double.parseDouble(fundsInput.getText().toString()), "holder");
